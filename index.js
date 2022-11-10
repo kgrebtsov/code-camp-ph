f5b97f90268468b9643262d23b87fbc760c8d988
Kirill Grebtsov <grebtsov@gmail.com>
2022-11-10 09:36:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName + '!')
+}
+let userName = prompt('Whats your name?');
+let userLastName = prompt('Whats your last nanme?')
+greet(userName, userLastName)
+
+
+
+
+
